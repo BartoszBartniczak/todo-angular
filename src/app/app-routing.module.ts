b0fb678ca593e6ca54134b61f213335bcb2d084f
Bartosz Bartniczak <bartoszbartniczak@gmail.com>
2019-07-20 15:05:30 +0200

    Simple routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {ErrorComponent} from './error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', redirectTo: 'tasks', pathMatch: 'full'},
+    {path: 'tasks', loadChildren: () => import('./task/task.module').then(m => m.TaskModule)},
+    {path: '**', component: ErrorComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes, {enableTracing: true})],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
